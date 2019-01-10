ef628f17b6f17ed5c64a3bb0ec9fea70a28b792b
unknown <1078763967@qq.com>
2019-01-10 16:50:49 +0800

    delete no-use notes

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,4 @@
 declare module "*.vue" {
     import Vue from "vue";
-    // interface Vue {
-    //     $Message: any,
-    //     $Modal: any,
-    //     $ref: any
-    // }
-
     export default Vue;
 }
